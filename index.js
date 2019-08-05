0ac86031f0678542509580afc9feb7adfbb10b91
Georgios Giannoutsos Barkas <contact@nuc.gr>
2019-08-05 09:35:52 +0200

    Sort rules

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ module.exports = {
     'jest': true
   },
   'rules': {
-    'semi': ['error', 'never'],
-    'lines-between-class-members': 'off',
-    'no-else-return': 'off',
     'comma-dangle': ['error', 'never'],
     'function-paren-newline': 'off',
     'import/extensions': 'off',
@@ -19,21 +16,24 @@ module.exports = {
     'import/order': 'off',
     'import/prefer-default-export': 'off',
     'jsx-a11y/anchor-is-valid': 'warn',
-    'jsx-a11y/media-has-caption': 'off',
     'jsx-a11y/label-has-associated-control': 'warn',
+    'jsx-a11y/media-has-caption': 'off',
+    'lines-between-class-members': 'off',
     'no-debugger': 'warn',
+    'no-else-return': 'off',
     'no-extra-semi': 'error',
     'no-restricted-imports': ['error', { patterns: ['../*'] }],
     'no-restricted-modules': ['error', { patterns: ['../*'] }],
+    'prettier/prettier': 'error',
     'quote-props': ['error', 'consistent-as-needed'],
+    'react/destructuring-assignment': 'warn',
     'react/jsx-curly-brace-presence': 'off',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-wrap-multilines': 'off',
     'react/prefer-stateless-function': 'off',
     'react/sort-comp': ['error', { order: ['static-methods', 'instance-variables', 'lifecycle', 'everything-else', 'render'] }],
     'react/style-prop-object': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'react/destructuring-assignment': 'warn',
-    'react/jsx-wrap-multilines': 'off',
-    'prettier/prettier': 'error'
+    'semi': ['error', 'never']
   }
 }
