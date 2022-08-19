a845c6f0934b99f2bddef0f2f0f1c104ce654ede
Marek Augustynowicz <marek.augustynowicz@verkstedt.com>
2022-08-19 15:38:03 +0200

    Move all customisation to `with` functions

--- typescript-react.js
+++ typescript-react.js
@@ -1,14 +1,4 @@
 const withTypescript = require('./lib/withTypescript')
 const react = require('./react')
 
-const config = withTypescript(react)
-
-module.exports = {
-  ...config,
-  rules: {
-    ...config.rules,
-    // In typescript we don’t use prop types and default props, but
-    // instead rely on props’s interfaces and function default values
-    'react/require-default-props': 'off',
-  },
-}
+module.exports = withTypescript(react)
