a13db36d08f36fe70ae1062f5b8caaca0fd2ab5d
Marek Augustynowicz <marek.augustynowicz@verkstedt.com>
2024-09-10 11:25:43 +0200

    eslint --fix .

--- next.js
+++ next.js
@@ -1,6 +1,6 @@
 const base = require('./base')
-const withPrettier = require('./lib/withPrettier')
 const withNext = require('./lib/withNext')
+const withPrettier = require('./lib/withPrettier')
 const withReact = require('./lib/withReact')
 
 module.exports = withPrettier(withNext(withReact(base)))
