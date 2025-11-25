f62984c8d10d815e1d2da4ddce926008e93b2b90
Marek Augustynowicz <marek.augustynowicz@verkstedt.com>
2025-11-25 16:05:21 +0100

    feat(prettier): trailing commas

--- prettier/index.ts
+++ prettier/index.ts
@@ -3,6 +3,8 @@ import type { Config } from 'prettier';
 const config: Config = {
   // Use single quotes (') instead of double ones (") by default
   singleQuote: true,
+  // Add trailing commas
+  trailingComma: 'all',
 };
 
 export default config;
