662c904a57bd68bd7fa2f51edaf82790d9a62401
Marek Augustynowicz <marek.augustynowicz@verkstedt.com>
2025-12-23 15:12:56 +0100

    feat: lint package.json

---
+++ scripts/lint-package-json.ts
@@ -0,0 +1,178 @@
+#!/usr/bin/env node
+
+import { readFile } from 'node:fs/promises';
+import { dirname } from 'node:path';
+import { inspect, parseArgs, type ParseArgsOptionsConfig } from 'node:util';
+
+type PackageName = string;
+type PackageVersionSpec = string;
+
+interface PackageJson {
+  name: PackageName;
+  peerDependencies?: Record<PackageName, PackageVersionSpec>;
+  peerDependenciesMeta?: Record<
+    PackageName,
+    { optional: boolean; note?: string }
+  >;
+  devDependencies?: Record<PackageName, PackageVersionSpec>;
+  dependencies?: Record<PackageName, PackageVersionSpec>;
+}
+
+type ParseArgsOptionsWithDescription = Record<
+  string,
+  ParseArgsOptionsConfig[string] & {
+    description: string;
+  }
+>;
+
+const options: ParseArgsOptionsWithDescription = {
+  help: {
+    type: 'boolean',
+    short: 'h',
+    description: 'Show this help message',
+  },
+};
+
+function shouldUseColours(): boolean {
+  if (process.env.FORCE_COLOR === '1') {
+    return true;
+  } else if (process.env.NO_COLOR === '1') {
+    return false;
+  } else {
+    return process.stdout.isTTY && process.stderr.isTTY;
+  }
+}
+
+const ansi = shouldUseColours()
+  ? {
+      reset: '\u001b[0m',
+      bold: '\u001b[1m',
+      dim: '\u001b[2m',
+    }
+  : {
+      reset: '',
+      bold: '',
+      dim: '',
+    };
+
+function printHelp() {
+  interface Opt {
+    flags: string;
+    description: string;
+  }
+  const opts: Array<Opt> = Object.entries(options).map(([name, opt]) => {
+    const flags = [`--${name}`, opt.short ? `-${opt.short}` : null]
+      .filter(Boolean)
+      .join(', ');
+    return { flags, description: opt.description };
+  });
+  const maxFlagLength = Math.max(...opts.map((opt) => opt.flags.length));
+
+  process.stdout.write(
+    [
+      `${ansi.bold}Check if things in package.json are in sync.${ansi.reset}`,
+      '',
+      `${ansi.bold}Usage:${ansi.reset} npm run lint:pkg ${ansi.dim}[OPTIONS]${ansi.reset}`,
+      '',
+      `${ansi.bold}Options:${ansi.reset}`,
+      ...opts.map(
+        (opt) => `  ${opt.flags.padEnd(maxFlagLength)}  ${opt.description}`,
+      ),
+      '',
+    ].join('\n'),
+  );
+}
+
+async function readPackageJson() {
+  const packageDir = dirname(import.meta.dirname);
+  const packageJsonPath = `${packageDir}/package.json`;
+  const packageJsonContent = await readFile(packageJsonPath, 'utf-8');
+  return JSON.parse(packageJsonContent) as PackageJson;
+}
+
+function lintPeerDependencies(packageJson: PackageJson) {
+  // Check if all peerDependencies are listed in devDependencies with
+  // the same version
+  const peerDeps = packageJson.peerDependencies ?? {};
+  const peerDepsMeta = packageJson.peerDependenciesMeta ?? {};
+  const devDeps = packageJson.devDependencies ?? {};
+  const errors: Array<string> = [];
+
+  for (const [peerDep, peerVersion] of Object.entries(peerDeps)) {
+    const devVersion = devDeps[peerDep];
+    if (!devVersion) {
+      errors.push(
+        `Peer dependency "${peerDep}" is missing in devDependencies.`,
+      );
+    } else if (!peerVersion.startsWith('>=')) {
+      errors.push(
+        `Peer dependency "${peerDep}" should use '>=' version specifier, found "${peerVersion}".`,
+      );
+    } else if (!devVersion.startsWith('^')) {
+      errors.push(
+        `Dev dependency "${peerDep}" should use '^' version specifier, found "${devVersion}".`,
+      );
+    } else if (devVersion.replace('^', '') !== peerVersion.replace('>=', '')) {
+      errors.push(
+        `Version mismatch for "${peerDep}": peer "${peerVersion}", dev "${devVersion}".`,
+      );
+    }
+
+    if (!(peerDep in peerDepsMeta)) {
+      errors.push(
+        `Peer dependency "${peerDep}" is missing in peerDependenciesMeta.`,
+      );
+    }
+  }
+
+  for (const [metaDep, meta] of Object.entries(peerDepsMeta)) {
+    if (!(metaDep in peerDeps) && !meta.optional) {
+      errors.push(
+        `peerDependenciesMeta entry "${metaDep}" is not listed in peerDependencies and is not marked as optional.`,
+      );
+    }
+
+    if (meta.optional && !meta.note) {
+      errors.push(
+        `peerDependenciesMeta entry "${metaDep}" is marked as optional but is missing a note explaining when it should be included.`,
+      );
+    }
+  }
+
+  if (errors.length) {
+    for (const error of errors) {
+      process.stderr.write(`- ${error}\n`);
+    }
+  }
+
+  return errors.length === 0;
+}
+
+async function main() {
+  const { values } = parseArgs({
+    options,
+    allowPositionals: false,
+  });
+
+  if (values.help) {
+    printHelp();
+    process.exit(0);
+  }
+
+  let hasErrors = false;
+
+  const packageJson = await readPackageJson();
+  hasErrors ||= !lintPeerDependencies(packageJson);
+
+  if (hasErrors) {
+    process.exit(1);
+  } else {
+    process.stdout.write(`${ansi.bold}package.json looks fine${ansi.reset}\n`);
+    process.exit(0);
+  }
+}
+
+main().catch((error: unknown) => {
+  process.stderr.write(`${inspect(error)}\n`);
+  process.exit(1);
+});
